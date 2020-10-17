1ba2cc0267ee29f58cab55573529816433f2dd22
philipd <philip.dickau@gmail.com>
2020-10-17 00:31:05 -0600

    Create benchmarking utility

---
+++ benchmark.js
@@ -0,0 +1,49 @@
+const benchmark = (callback) => {
+  console.time(callback.name);
+  const a = callback();
+  console.timeEnd(callback.name);
+  console.log("Returns: ", a);
+};
+
+const doXTimes = (callback, x) => {
+  const functionToTest = () => {
+    for (let i = 0; i < x; i++) {
+      callback();
+    }
+  };
+
+  // Rename the return function to have the same name as the callback
+  Object.defineProperty(functionToTest, "name", { value: callback.name });
+
+  return functionToTest;
+};
+
+const tasks = {
+  declareVariable: () => {
+    let a;
+  },
+
+  declareInteger: () => {
+    let a = 1;
+  },
+
+  declareEmptyArray: () => {
+    let a = [];
+  },
+
+  declareFloat: () => {
+    let a = 1.1;
+  },
+
+  createSmallArray: () => {
+    let a = [];
+    for(let i = 0; i<10; i++){
+      a.push(i);
+    }
+  },
+};
+
+for (let task in tasks) {
+  const bigTask = doXTimes(tasks[task], Math.pow(10, 8))
+  benchmark(bigTask);
+}
---
+++ draft.md
@@ -0,0 +1,4 @@
+## Introduction
+If you've spent more than a couple of hours in your life coding, then you've almost certainly heard of arrays. Arrays are just one example of a *data structure*--a way of collecting multiple pieces of data together in an organized way so we can use them more effectively. Objects, hashes, and dictionaries are other examples of data structures. In this article, I'll take a quick look at a data structure that you might be less familiar with: the *linked list*.
+
+## Caveat
